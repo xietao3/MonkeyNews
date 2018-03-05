8e5716e23cd0e1bb2483c32292b71df4927130ed
xietao <xietao3@qq.com>
2018-03-05 21:52:21 +0800

    优化TabBar

--- js/Pages/MKTabBar.js
+++ js/Pages/MKTabBar.js
@@ -19,16 +19,30 @@ import {
 // import MKColor from '../Config/MKColor'
 
 
-const HomeTab = 'homeTab'
-const CategoryTab = 'CategoryTab'
-const UserTab = 'UserTab'
+const tabBarConfig = {
+    home: {
+        name:"热点",
+        icon:require('../../src/home.png'),
+        selected:'HomeTab'
+    },
+    category: {
+        name:"分类",
+        icon:require('../../src/home.png'),
+        selected:'CategoryTab'
+    },
+    user: {
+        name:"个人中心",
+        icon:require('../../src/home.png'),
+        selected:'UserTab'
+    }
+}
 
 export default class App extends Component {
 
     constructor (props) {
         super(props);
         this.state = {
-            selectedTab: HomeTab
+            selectedTab: tabBarConfig.home.selected
         };
 
     }
@@ -42,22 +56,37 @@ export default class App extends Component {
         );
     }
 
+    _renderTabBarItem(tabBarConfigItem, page) {
+        return (
+            <TabBarIOS.Item
+                title={tabBarConfigItem.name}
+                icon={tabBarConfigItem.icon}
+                selected={this.state.selectedTab === tabBarConfigItem.selected}
+                onPress={() => {
+                    this.setState({
+                        selectedTab: tabBarConfigItem.selected,
+                    });
+                }}
+            >
+                {page}
+            </TabBarIOS.Item>
+        );
+    }
+
 
     render() {
         return (
            <TabBarIOS>
-               <TabBarIOS.Item
-                   title="热点"
-                   icon={require('../../src/home.png')}
-                   selected={this.state.selectedTab === HomeTab}
-                   onPress={() => {
-                       this.setState({
-                           selectedTab: HomeTab,
-                       });
-                   }}
-               >
-                   {this._renderContent("#ff0000","home")}
-               </TabBarIOS.Item>
+               {this._renderTabBarItem(tabBarConfig.home, (
+                   <View style={[styles.tabContent, {backgroundColor:'#ff0000'}]}>
+                       <Text style={styles.tabText}>diddsdasds</Text>
+                   </View>
+               ))}
+
+
+               {/*{this._renderTabBarItem(tabBarConfig.home, this._renderContent("#ff0000","home"))}*/}
+               {this._renderTabBarItem(tabBarConfig.category, this._renderContent("#00ff00","分类页面"))}
+               {this._renderTabBarItem(tabBarConfig.user, this._renderContent("#ff00ff","个人中心页面"))}
            </TabBarIOS>
         );
 
